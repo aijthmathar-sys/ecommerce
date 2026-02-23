ee34c102229bae26f1527e6d5cc5fbf54964d758
mathar <you@example.com>
2026-02-24 00:30:36 +0530

    lj

--- ecommerce/src/main/java/com/example/ecommerce/security/SecurityConfig.java
+++ ecommerce/src/main/java/com/example/ecommerce/security/SecurityConfig.java
@@ -1,6 +1,5 @@
-package com.example.ecommerce.config;
+package com.example.ecommerce.security;
 
-import com.example.ecommerce.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
